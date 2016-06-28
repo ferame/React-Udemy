c0a388f1ce9be1519c491824e16bf761a395617a
ferame <alisauskas.j@gmail.com>
2016-06-28 15:22:59 +0100

    Added First Challenge folder, still no part of this challenge is done

---
+++ FirstChallenge/server.js
@@ -0,0 +1,9 @@
+var express = require('express'); //The name of the module
+
+//Create our app
+var app = express();
+
+app.use(express.static('public')); //telling which server we want to use
+app.listen(3000, function(){
+  console.log('Express server is up on port 3001');
+}); //starts server
